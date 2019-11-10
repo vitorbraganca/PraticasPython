a64fe06c3e9bfe39617a404cdf2d065c50462a47
Vitor Bragança <vitor_braganca@unigranrio.br>
2019-11-10 15:21:26 -0300

    Atualizacao
    Agora, é possível listar todos os clientes cadastrados no sistema com a nova funcao implementada.
    Também foi adicionado um menu para diferentes niveis de usuario (Gerente e Vendedor) onde terão diferentes opções.

--- Cadastros/GUI.py
+++ Cadastros/GUI.py
@@ -1,31 +1,42 @@
-from Cadastro import cadastro_clientes
+from Cadastro.cadastro_clientes import consulta_cliente, cadastro_cliente
+from Testes.listar import listar_clientes
+
 while True:
-    print("""1 - Cadastrar
-2- Consultar
-3- Listar todos
-4- Editar dados
-5- Sair""")
-    op = int(input('OPCAO: '))
-    if op == 1:
-        cadastro_cliente()
-    elif op == 2:
+    print("""1- Usuario: Gerente
+2- Usuario: Funcionario""")
+    user = int(input('Opcao de usuario: '))
+    if user == 1:
+        print('Funcionalidade em desenvolvimento...')
+        print('Voltando para o menu...')
+    elif user == 2:
+        print('Logando como funcionario.')
         while True:
-            consulta_cliente()
-            resp = input('Deseja consultar novamente? ').upper()  # Deixando em caixa alta para reduzir possibilidades
-            if resp.startswith('N'):
-                print('Consulta encerrada ')
+            print("""1 - Cadastrar cliente
+2- Consultar cliente
+3- Listar todos clientes
+4- Editar dados de clientes
+5- Sair""")
+            op = int(input('OPCAO: '))
+            if op == 1:
+                cadastro_cliente()
+            elif op == 2:
+                while True:
+                    consulta_cliente()
+                    resp = input('Deseja consultar novamente? ').upper()  # Deixando em caixa alta para reduzir possibilidades
+                    if resp.startswith('N'):
+                        print('Consulta encerrada ')
+                        break
+                    else:
+                        print('Nova consulta... ')
+            elif op == 3:
+                print('Listando todos os dados cadastrados...')
+                listar_clientes()
+                print('Voltando para o menu...')
+            elif op == 4:
+                print('Funcionalidade em desenvolvimento...')
+                print('Voltando para o menu...')
+            elif op == 5:
+                print('Saindo...')
                 break
             else:
-                print('Nova consulta... ')
-    elif op == 3:
-        print('Funcionalidade em desenvolvimento...')
-        print('Voltando para o menu...')
-    elif op == 4:
-        print('Funcionalidade em desenvolvimento...')
-        print('Voltando para o menu...')
-    elif op == 5:
-        print('Saindo...')
-        break
-    else:
-        print('Funcao nao encontrada. Tente novamente.')
-input('Pressione qualquer tecla para sair...')
+                print('Funcao nao encontrada. Tente novamente.')
